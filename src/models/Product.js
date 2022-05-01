a01a6d6cbb40665a6ab5ba78b9a7766b2d9dfa2a
NilsHolthey <nils.holthey@gmx.de>
2022-05-01 21:15:40 +0200

    Add Category in products

--- src/models/Product.js
+++ src/models/Product.js
@@ -3,6 +3,9 @@ import Category from "./Category";
 
 const productSchema = new mongoose.Schema({
   name: String,
+  description: String,
+  tags: Array,
+  price: Number,
   category: { type: mongoose.Schema.Types.ObjectId, ref: "Category" },
 });
 
